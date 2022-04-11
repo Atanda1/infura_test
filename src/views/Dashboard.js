250483956336252b9f4f3cc99c9e99e6d4bfaa6f
David Atanda <david@Davids-MacBook-Pro.local>
2022-04-11 06:17:54 +0100

    dashboard UI completed - block UI and button

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -41,7 +41,59 @@ const Dashboard = () => {
       </div>
 
       <div className="explorer">
-        <div className="explorer__header"></div>
+        <header className="explorer__header">
+          <div className="explorer__header__info">
+            <div className="explorer__header__info__option">
+              <h3 className="explorer__header__info__option__text">CURRENT BLOCK</h3>
+              <div className="explorer__header__info__option__number">
+                <span>
+                  <h1>8,243,123</h1>
+                </span>
+              </div>
+            </div>
+            <div className="explorer__header__info__option">
+              <h3 className="explorer__header__info__option__text">AVERAGE GAS PRICE</h3>
+              <div className="explorer__header__info__option__number">
+                <span>
+                  <h1>87</h1>gwei
+                </span>
+              </div>
+            </div>
+            <div className="explorer__header__info__option">
+              <h3 className="explorer__header__info__option__text">AVERAGE BLOCK PRICE</h3>
+              <div className="explorer__header__info__option__number">
+                <span>
+                  <h1>8.2</h1>mgas
+                </span>
+              </div>
+            </div>
+            <div className="explorer__header__info__option">
+              <h3 className="explorer__header__info__option__text">AVERAGE BLOCK FULLNESS</h3>
+              <div className="explorer__header__info__option__number">
+                <span>
+                  <h1>88%</h1>
+                </span>
+              </div>
+            </div>
+          </div>
+        </header>
+
+        <div className="explorer__blocks">
+          <div className="explorer__blocks__wrapper">
+            <div className="explorer__blocks__wrapper__item">
+              <div>
+                <h3>#8, 243, 123</h3>
+                <h5>mined 5s ago</h5>
+              </div>
+              <h3>82 TXs</h3>
+            </div>
+            <div className="explorer__blocks__wrapper__item"></div>
+            <div className="explorer__blocks__wrapper__item"></div>
+            <div className="explorer__blocks__wrapper__item"></div>
+            <div className="explorer__blocks__wrapper__item"></div>
+            <div className="explorer__blocks__wrapper__item"></div>
+          </div>
+        </div>
       </div>
     </div>
   );
