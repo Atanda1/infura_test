bc049500ee76a676733bc2b12dcd192d2bfab259
David Atanda <atandadave@gmail.com>
2022-04-13 13:18:55 +0100

    Block UI bug

--- src/components/tooltip/tooltip.js
+++ src/components/tooltip/tooltip.js
@@ -25,57 +25,53 @@ const Tooltip = ({ transactionHash }) => {
     }
   };
   return (
-    <>
-      <ReactTooltip
-        className="transaction__tooltip"
-        backgroundColor="#ffff"
-        id="transaction"
-        place="right"
-        effect="solid">
-        <div className="fuck">
-          <div className="transaction__tooltip__location">
-            <div className="transaction__tooltip__location__sender">
-              <h3 className="transaction__tooltip__location__sender__title">FROM</h3>
+    <ReactTooltip
+      className="transaction__tooltip"
+      backgroundColor="#ffff"
+      id="transaction"
+      place="right"
+      effect="solid">
+      <div className="transaction__tooltip__location">
+        <div className="transaction__tooltip__location__sender">
+          <h3 className="transaction__tooltip__location__sender__title">FROM</h3>
 
-              <div className="transaction__tooltip__location__sender__address">
-                {newTransaction.from ? (
-                  `${newTransaction.from.slice(0, 4) + '...' + newTransaction.from.slice(-4)}`
-                ) : (
-                  <h3>Loading</h3>
-                )}
-              </div>
-            </div>
-            <div className="transaction__tooltip__location__receiver">
-              <h3 classame="transaction__tooltip__location__receiver__title">TO</h3>
-
-              <div className="transaction__tooltip__location__receiver__address">
-                {newTransaction.to ? (
-                  `${newTransaction.from.slice(0, 4) + '...' + newTransaction.from.slice(-4)}`
-                ) : (
-                  <h3>Loading</h3>
-                )}
-              </div>
-            </div>
-          </div>
-
-          <div className="transaction__tooltip__value">
-            <div className="transaction__tooltip__value__transaction">
-              <h3 className="transaction__tooltip__value__transaction__title">VALUE</h3>
-
-              <div className="transaction__tooltip__value__transaction__amount">
-                {newTransaction.value ? `${getTransactionValue(newTransaction)}` : <h3>Loading</h3>}
-              </div>
-            </div>
-
-            <div className="transaction__tooltip__value__cash">
-              <h3>$1452</h3>
-              <span>@</span>
-              <h3>$90.10</h3>
-            </div>
+          <div className="transaction__tooltip__location__sender__address">
+            {newTransaction.from ? (
+              `${newTransaction.from.slice(0, 4) + '...' + newTransaction.from.slice(-4)}`
+            ) : (
+              <h3>Loading</h3>
+            )}
           </div>
         </div>
-      </ReactTooltip>
-    </>
+        <div className="transaction__tooltip__location__receiver">
+          <h3 classame="transaction__tooltip__location__receiver__title">TO</h3>
+
+          <div className="transaction__tooltip__location__receiver__address">
+            {newTransaction.to ? (
+              `${newTransaction.from.slice(0, 4) + '...' + newTransaction.from.slice(-4)}`
+            ) : (
+              <h3>Loading</h3>
+            )}
+          </div>
+        </div>
+      </div>
+
+      <div className="transaction__tooltip__value">
+        <div className="transaction__tooltip__value__transaction">
+          <h3 className="transaction__tooltip__value__transaction__title">VALUE</h3>
+
+          <div className="transaction__tooltip__value__transaction__amount">
+            {newTransaction.value ? `${getTransactionValue(newTransaction)}` : <h3>Loading</h3>}
+          </div>
+        </div>
+
+        <div className="transaction__tooltip__value__cash">
+          <h3>$1452</h3>
+          <span>@</span>
+          <h3>$90.10</h3>
+        </div>
+      </div>
+    </ReactTooltip>
   );
 };
 
