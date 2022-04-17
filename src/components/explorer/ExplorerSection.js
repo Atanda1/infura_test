3ef49554b76cd2970254e59a836bd42cf33e650f
David Atanda <david@Davids-MacBook-Pro.local>
2022-04-17 15:52:42 +0100

    switched to using infura to get block number

--- src/components/explorer/ExplorerSection.js
+++ src/components/explorer/ExplorerSection.js
@@ -5,6 +5,7 @@ import { useEffect, useState } from 'react';
 import TopBarProgress from 'react-topbar-progress-indicator';
 import { web3 } from '../../web3/index';
 import { config } from '../../loader/topbarConfig';
+import axios from 'axios';
 
 TopBarProgress.config(config);
 
@@ -15,21 +16,28 @@ const ExplorerSection = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    web3.eth.getBlockNumber(async (error, result) => {
-      if (!error) {
-        const allBlocks = [];
-        for (var i = 0; i < 20; i++) {
-          var block = await web3.eth.getBlock(result - i);
-          allBlocks.push(block);
-        }
-        await setBlocks(allBlocks);
-        await setIsLoading(false);
-      }
-      if (error) {
-        console.log(error);
-      }
-    });
+    axios
+      .post(process.env.REACT_APP_PROVIDER, {
+        jsonrpc: '2.0',
+        method: 'eth_blockNumber',
+        params: [],
+        id: 1
+      })
+      .then((res) => {
+        getLatestestBlock(parseInt(res.data.result, 16));
+      })
+      .catch((err) => console.log(err));
   }, []);
+
+  const getLatestestBlock = async (result) => {
+    const allBlocks = [];
+    for (var i = 0; i < 20; i++) {
+      var block = await web3.eth.getBlock(result - i);
+      allBlocks.push(block);
+    }
+    await setBlocks(allBlocks);
+    await setIsLoading(false);
+  };
 
   const loadMoreBlocks = async () => {
     setLoadingMore('LOADING MORE...');
